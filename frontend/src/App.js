949a883fd7d612f187d55b2a56ebf855cb7a7719
Carmen Chesoi <chesoicarmen@gmail.com>
2019-09-01 22:51:12 +0100

    Added inputs for numbers. Cleanup of index'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Calculator extends React.Component {
+  state = {
+    operand1: Number,
+    operand2: Number,
+    result: Number
+  }
+
+  componentDidMount() {
+    this.setState({ operand1: 0 });
+    this.setState({ operand2: 1 });
+  }
+
+  handleChange1(event) {
+    const value = event.target.value;
+    this.setState({operand1: value});
+  }
+  handleChange2(event) {
+    const value = event.target.value;
+    this.setState({operand2: value});
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          <label>First number:</label>
+          <input type="text"
+                 pattern="[0-9]*"
+                 onInput={this.handleChange1.bind(this)}
+                 value={this.state.operand1}/>
+        </div>
+
+        <div>
+          <label>Second number:</label>
+          <input type="text"
+                 pattern="[0-9]*"
+                 onInput={this.handleChange2.bind(this)}
+                 value={this.state.operand2}/>
+        </div>
+
+        <div>
+          <label>Operation:</label>
+
+        </div>
+
+        <div>
+          <p>Your result is: {this.state.result}</p>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Calculator;
